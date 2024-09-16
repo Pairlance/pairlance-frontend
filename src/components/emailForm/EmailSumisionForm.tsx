320c05a7f2f84449e682c8094eb0e5d18d124b3d
Xcoder2023 <146191492+Xcoder2023@users.noreply.github.com>
2024-09-15 22:39:38 -0800

    enhancing responsiveness

--- src/components/emailForm/EmailSumisionForm.tsx
+++ src/components/emailForm/EmailSumisionForm.tsx
@@ -17,8 +17,8 @@ const EmailSubmissionForm = () => {
   };
 
   return (
-    <div className="flex justify-center items-center border border-[#D0D2D6] h-[726px] w-[700px] mx-auto rounded-[16px] my-10" style={{ fontFamily: "Lato" }}>
-      <div className="bg-white p-8 max-w-md flex flex-col gap-5">
+    <div className="flex justify-center items-center lg:border border-[#D0D2D6] lg:h-[726px] lg:w-[700px] mx-auto rounded-[16px] my-10 " style={{ fontFamily: "Lato" }}>
+      <div className="bg-white p-8 max-w-md flex flex-col gap-5 ">
         <h2 className="text-[24px] font-bold text-center leading-[30.17px]" style={{ fontFamily: "Merriweather" }}>Provide Your Email</h2>
         <p className="text-center text-[#5F6774] text-[18px] leading-[21.6px] mb-8">
           Enter your email address to receive the selected candidates' CVs directly in your inbox.
@@ -71,12 +71,12 @@ const EmailSubmissionForm = () => {
             >
               Your email has been successfully submitted!
             </h2>
-            <p className="text-center text-[#5F6774] text-[18px] mb-6 leading-[21.6px] w-[476px]">
+            <p className="text-center text-[#5F6774] text-[18px] mb-6 leading-[21.6px] lg:w-[476px]">
               The CVs of the selected candidates will be sent to the provided email address shortly.
             </p>
-            <div className="flex space-x-4">
-              <Button type="default" onClick={() => window.location.href = "/"} className="border border-[#1E3A8A] text-[#1E3A8A] p-[16px] h-[54px] rounded-[16px]">Return to Homepage</Button>
-              <Button type="primary" onClick={() => window.location.href = "/matching"} className="bg-[#1E3A8A] text-[#FFFFFF] p-[16px] rounded-[16px] h-[54px]">Find More Candidates</Button>
+            <div className="flex flex-col lg:flex-row lg:space-x-4 gap-5 lg:gap-0 w-full">
+              <Button type="default" onClick={() => window.location.href = "/"} className="border border-[#1E3A8A] text-[#1E3A8A] p-[16px] h-[54px] rounded-[16px] w-full">Return to Homepage</Button>
+              <Button type="primary" onClick={() => window.location.href = "/matching"} className="bg-[#1E3A8A] text-[#FFFFFF] p-[16px] rounded-[16px] h-[54px] w-full">Find More Candidates</Button>
             </div>
           </div>
         </Modal>
